02426ee70733ad08b0a5e43e25ca74faf340aee0
Kostya4757 <105310560+Kostya4757@users.noreply.github.com>
2023-01-13 00:48:27 +0300

    Add files via upload

---
+++ assets/preload/stages/Cross.lua
@@ -0,0 +1,485 @@
+function onCreate()
+	makeLuaSprite('Brim', 'Maps/Brimstone', -250, -250);
+	setScrollFactor('Brim', 1, 1);
+	addLuaSprite('Brim', false);
+	setScrollFactor('Brim', 1, 1);
+	scaleObject('Brim', 4.26, 3.9)	
+	setProperty('Brim.antialiasing',false)
+
+	makeLuaSprite('Hypno', 'Maps/Hypno', -500, -650);
+	setScrollFactor('Hypno', 1, 1);
+	addLuaSprite('Hypno', false);
+	setScrollFactor('Hypno', 1, 1);
+	scaleObject('Hypno', 0.75, 0.75)
+
+	makeLuaSprite('Hell', 'Maps/Hell', 0, -650);
+	setScrollFactor('Hell', 1, 1);
+	addLuaSprite('Hell', false);
+	setScrollFactor('Hell', 1, 1);
+	scaleObject('Hell', 1, 1)
+
+	makeLuaSprite('Moni', 'Maps/Moni', 1050, -650);
+	setScrollFactor('Moni', 1, 1);
+	addLuaSprite('Moni', false);
+	setScrollFactor('Moni', 1, 1);
+	scaleObject('Moni', 1, 1.4)
+
+	makeLuaSprite('Shitno1', 'Maps/Shitno', -500, 0);
+	setScrollFactor('Shitno1', 1, 1);
+	addLuaSprite('Shitno1', false);
+	setScrollFactor('Shitno1', 1, 1);
+	scaleObject('Shitno1', 1, 1)
+
+	makeLuaSprite('Shitno2', 'Maps/Shitno', 1150, 0);
+	setScrollFactor('Shitno2', 1, 1);
+	addLuaSprite('Shitno2', false);
+	setScrollFactor('Shitno2', 1, 1);
+	scaleObject('Shitno2', 1, 1)
+
+	makeLuaSprite('Bendy', 'Maps/Bendy', -900, -900);
+	setScrollFactor('Bendy', 1, 1);
+	addLuaSprite('Bendy', false);
+	setScrollFactor('Bendy', 1, 1);
+	scaleObject('Bendy', 1.25, 1)
+
+	makeLuaSprite('Cuphead', 'Maps/Cuphead', -600, -825);
+	setScrollFactor('Cuphead', 1, 1);
+	addLuaSprite('Cuphead', false);
+	setScrollFactor('Cuphead', 1, 1);
+	scaleObject('Cuphead', 1.31, 1.15)
+
+	makeLuaSprite('Monika', 'Maps/Monika', 50, -650);
+	setScrollFactor('Monika', 1, 1);
+	addLuaSprite('Monika', false);
+	setScrollFactor('Monika', 1, 1);
+	scaleObject('Monika', 1, 1)
+
+	makeAnimatedLuaSprite('Poem', 'Maps/Poem', 50, -650);
+	setScrollFactor('Poem', 1, 1);
+	addLuaSprite('Poem', false);
+	setScrollFactor('Poem', 1, 1);
+	scaleObject('Poem', 1.35, 1.35)
+
+	makeLuaSprite('Sayori', 'Maps/Sayori', 50, -550);
+	setScrollFactor('Sayori', 1, 1);
+	addLuaSprite('Sayori', false);
+	setScrollFactor('Sayori', 1, 1);
+	scaleObject('Sayori', 1.25, 1.15)
+
+	makeAnimatedLuaSprite('Static', 'Maps/Static', 50, -650);
+	setScrollFactor('Static', 1, 1);
+	addLuaSprite('Static', false);
+	setScrollFactor('Static', 1, 1);
+	scaleObject('Static', 1.35, 1.35)
+
+	makeLuaSprite('Natsuki', 'Maps/Natsuki', 50, -650);
+	setScrollFactor('Natsuki', 1, 1);
+	addLuaSprite('Natsuki', false);
+	setScrollFactor('Natsuki', 1, 1);
+	scaleObject('Natsuki', 1, 1)
+
+	makeLuaSprite('Yuri', 'Maps/Yuri', 50, -650);
+	setScrollFactor('Yuri', 1, 1);
+	addLuaSprite('Yuri', false);
+	setScrollFactor('Yuri', 1, 1);
+	scaleObject('Yuri', 1, 1)
+
+	makeLuaSprite('Cold', 'Maps/Cold', -875, -650);
+	setScrollFactor('Cold', 1, 1);
+	addLuaSprite('Cold', false);
+	setScrollFactor('Cold', 1, 1);
+	scaleObject('Cold', 1, 1)
+
+	makeAnimatedLuaSprite('Sans', 'Maps/Sans', -600, -600);
+	setScrollFactor('Sans', 1, 1);
+	addLuaSprite('Sans', false);
+	setScrollFactor('Sans', 1, 1);
+	scaleObject('Sans', 0.84, 0.88)
+
+	makeAnimatedLuaSprite('Static2', 'Maps/Static', 200, -600);
+	setScrollFactor('Static2', 1, 1);
+	addLuaSprite('Static2', false);
+	setScrollFactor('Static2', 1, 1);
+	scaleObject('Static2', 1, 0.88)
+
+	makeLuaSprite('Nat', 'Maps/Nat', 200, -600);
+	setScrollFactor('Nat', 1, 1);
+	addLuaSprite('Nat', false);
+	setScrollFactor('Nat', 1, 1);
+	scaleObject('Nat', 1, 1)
+
+	makeAnimatedLuaSprite('Poem2', 'Maps/Poem', -200, -650);
+	setScrollFactor('Poem2', 1, 1);
+	addLuaSprite('Poem2', false);
+	setScrollFactor('Poem2', 1, 1);
+	scaleObject('Poem2', 1.35, 1.35)
+
+	makeLuaSprite('Sayo', 'Maps/Sayori', -200, -550);
+	setScrollFactor('Sayo', 1, 1);
+	addLuaSprite('Sayo', false);
+	setScrollFactor('Sayo', 1, 1);
+	scaleObject('Sayo', 1.25, 1.15)
+
+	makeLuaSprite('Cup', 'Maps/Cup', -600, -600);
+	setScrollFactor('Cup', 1, 1);
+	addLuaSprite('Cup', false);
+	setScrollFactor('Cup', 1, 1);
+	scaleObject('Cup', 0.83, 0.85)
+
+	makeLuaSprite('BendyNight', 'Maps/BendyNight', -550, -600);
+	setScrollFactor('BendyNight', 1, 1);
+	addLuaSprite('BendyNight', false);
+	setScrollFactor('BendyNight', 1, 1);
+	scaleObject('BendyNight', 0.75, 0.85)
+
+	makeLuaSprite('Ben', 'Maps/Ben', -600, -300);
+	setScrollFactor('Ben', 1, 1);
+	addLuaSprite('Ben', false);
+	setScrollFactor('Ben', 1, 1);
+	scaleObject('Ben', 1, 0.85)
+
+	makeAnimatedLuaSprite('Barrer1', 'Maps/Barrer', 400, -650);
+	setScrollFactor('Barrer1', 1, 1);
+	addLuaSprite('Barrer1', true);
+	setScrollFactor('Barrer1', 1, 1);
+	scaleObject('Barrer1', 1, 1.85)
+
+	makeAnimatedLuaSprite('Barrer2', 'Maps/Barrer', -300, -650);
+	setScrollFactor('Barrer2', 1, 1);
+	addLuaSprite('Barrer2', true);
+	setScrollFactor('Barrer2', 1, 1);
+	scaleObject('Barrer2', 1, 1.85)
+
+	makeAnimatedLuaSprite('Barrer3', 'Maps/Barrer', -435, -650);
+	setScrollFactor('Barrer3', 1, 1);
+	addLuaSprite('Barrer3', true);
+	setScrollFactor('Barrer3', 1, 1);
+	scaleObject('Barrer3', 1, 1.85)
+
+	makeAnimatedLuaSprite('Lullaby', 'Maps/Lullaby', -50, -400);
+	setScrollFactor('Lullaby', 1, 1);
+	addLuaSprite('Lullaby', true);
+	setScrollFactor('Lullaby', 1, 1);
+	scaleObject('Lullaby', 0.75, 0.75)
+
+	makeLuaSprite('Jojo', 'Maps/Jotared', -250, -250);
+	setScrollFactor('Jojo', 1, 1);
+	addLuaSprite('Jojo', false);
+	setScrollFactor('Jojo', 1, 1);
+	scaleObject('Jojo', 0.5, 0.5)
+
+	makeLuaSprite('White', 'Maps/White', -1000, -1000);
+	setScrollFactor('White', 1, 1);
+	addLuaSprite('White', true);
+	setScrollFactor('White', 1, 1);
+	scaleObject('White', 10, 10)
+
+	makeLuaSprite('Marcos', 'Maps/Marcos', -1000, -1000);
+	setScrollFactor('Marcos', 1, 1);
+	addLuaSprite('Marcos', true);
+	setScrollFactor('Marcos', 1, 1);
+	scaleObject('Marcos', 10, 10)
+
+	makeLuaSprite('Black', 'Maps/Black', -450, -200);
+	setScrollFactor('Black', 1, 1);
+	addLuaSprite('Black', true);
+	setScrollFactor('Black', 1, 1);
+	scaleObject('Black', 10, 1.2)
+
+
+
+	addAnimationByPrefix('Lullaby','idle', 'StaticHypno',24,true)
+	addAnimationByPrefix('Barrer1','idle', 'pixelthing',24,true)
+	addAnimationByPrefix('Barrer2','idle', 'pixelthing',24,true)
+	addAnimationByPrefix('Barrer3','idle', 'pixelthing',24,true)
+	addAnimationByPrefix('Poem','idle', 'poemtransition',24,true)
+	addAnimationByPrefix('Poem2','idle', 'poemtransition',24,true)
+	addAnimationByPrefix('Static','idle', 'HomeStatic',24,true)
+	addAnimationByPrefix('Static2','idle', 'HomeStatic',24,true)
+	addAnimationByPrefix('Sans','idle', 'Normal',24,true)
+
+
+
+	setProperty('Marcos.visible', false)
+	setProperty('Black.visible', false)
+	setProperty('Cold.visible', false)
+	setProperty('Bendy.visible', false)
+	setProperty('Cuphead.visible', false)
+	setProperty('Shitno1.visible', false)
+	setProperty('Shitno2.visible', false)
+	setProperty('Black.visible', false)
+	setProperty('Moni.visible', false)
+	setProperty('Monika.visible', false)
+	setProperty('Sayori.visible', false)
+	setProperty('Poem.visible', false)
+	setProperty('Natsuki.visible', false)
+	setProperty('Static.visible', false)
+	setProperty('Yuri.visible', false)
+	setProperty('Nat.visible', false)
+	setProperty('Static2.visible', false)
+	setProperty('Sans.visible', false)
+	setProperty('Sayo.visible', false)
+	setProperty('Poem2.visible', false)
+	setProperty('Cup.visible', false)
+	setProperty('BendyNight.visible', false)
+	setProperty('Ben.visible', false)
+	setProperty('Barrer1.visible', false)
+	setProperty('Barrer2.visible', false)
+	setProperty('Barrer3.visible', false)
+	setProperty('Lullaby.visible', false)	
+	setProperty('White.visible', false)
+	setProperty('Hell.visible', false)
+	setProperty('Brim.visible', false)	
+	setProperty('Hypno.visible', false)
+	setProperty('Hell.visible', false)	
+end	
+
+
+
+	
+function onStepHit()
+  if curStep == 4 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+	doTweenAlpha('L', 'Lullaby', 0, 0.1)
+	doTweenAlpha('S1', 'Shitno1', 0, 0.1)
+	doTweenAlpha('S2', 'Shitno2', 0, 0.1)
+	doTweenAlpha('Y', 'Yuri', 0, 0.1)	
+ end
+  
+  if curStep == 403 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 404 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+  
+  if curStep == 784 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 785 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+
+  if curStep == 1036 then	
+	doTweenAlpha('L', 'Lullaby', 1, 0.0001)	
+  end
+  if curStep == 1038 then	
+	doTweenAlpha('L', 'Lullaby', 0, 0.25)	
+  end
+
+  if curStep == 1299 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 1300 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+
+  if curStep == 2192 then	
+	doTweenAlpha('B', 'Barrer2', 0, 0.0001)	
+  end
+
+  if curStep == 2299 then	
+	doTweenAlpha('B', 'Barrer2', 1, 1.5)	
+  end
+
+  if curStep == 2330 then	
+	doTweenAlpha('B', 'Barrer1', 0, 1.5)	
+	doTweenAlpha('M', 'Moni', 0, 1.5)
+  end
+
+  if curStep == 2836 then	
+	doTweenAlpha('B', 'Barrer2', 0, 0.5)
+  end
+
+  if curStep == 2965 then	
+	doTweenAlpha('S', 'Shitno1', 1, 0.1)	
+  end
+  if curStep == 3511 then	
+	doTweenAlpha('S', 'Shitno2', 1, 0.1)	
+  end
+
+  if curStep == 3539 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 3540 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+
+  if curStep == 4180 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 4182 then	
+	doTweenAlpha('W', 'White', 0, 1)	
+  end
+
+  if curStep == 4436 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 4437 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+
+  if curStep == 4962 then	
+	doTweenAlpha('N', 'Natsuki', 0, 5)	
+	doTweenAlpha('S', 'Static', 0, 5)
+  end
+
+  if curStep == 4692 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+  if curStep == 4693 then	
+	doTweenAlpha('W', 'White', 0, 0.1)	
+  end
+
+  if curStep == 5032 then	
+	doTweenAlpha('Y', 'Yuri', 1, 5)	
+  end
+  if curStep == 5237 then	
+	doTweenAlpha('W', 'White', 1, 0.5)	
+  end
+
+  if curStep == 5255 then	
+	doTweenAlpha('W', 'White', 0, 0.5)	
+  end
+
+  if curStep == 6164 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+
+  if curStep == 6165 then	
+	doTweenAlpha('W', 'White', 0, 1)	
+  end
+
+  if curStep == 6292 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+
+  if curStep == 6293 then	
+	doTweenAlpha('W', 'White', 0, 1)	
+  end
+
+  if curStep == 6420 then	
+	doTweenAlpha('W', 'White', 1, 0.1)	
+  end
+
+  if curStep == 6421 then	
+	doTweenAlpha('W', 'White', 0, 1)	
+  end
+end
+
+
+
+	
+function onEvent(name,value1,value2)	
+if name == 'Play Animation' then 
+	if value1 == 'White' then
+		setProperty('White.visible', true);
+		setProperty('Lullaby.visible', true);
+	end
+
+	if value1 == 'Brim' then
+		setProperty('Jojo.visible', false);
+		setProperty('Brim.visible', true);
+	end	
+
+	if value1 == 'Hypno' then
+		setProperty('Brim.visible', false);
+		setProperty('Hypno.visible', true);		
+	end	
+
+	if value1 == 'Hell' then
+		setProperty('Hypno.visible', false);		
+		setProperty('Hell.visible', true);	
+	end		
+
+	if value1 == 'Moni' then
+		setProperty('Hell.visible', false);		
+		setProperty('Moni.visible', true);	
+		setProperty('Black.visible', true);
+		setProperty('Barrer1.visible', true);
+		setProperty('Barrer2.visible', true);
+	end
+
+	if value1 == 'Shitno' then
+		setProperty('Moni.visible', false);	
+		setProperty('Black.visible', false);
+		setProperty('Barrer1.visible', false);
+		setProperty('Barrer2.visible', false);
+		setProperty('Shitno1.visible', true);
+		setProperty('Shitno2.visible', true);
+	end
+
+	if value1 == 'Bendy' then
+		setProperty('Shitno1.visible', false);
+		setProperty('Shitno2.visible', false);
+		setProperty('Bendy.visible', true);
+	end
+
+	if value1 == 'Cuphead' then
+		setProperty('Bendy.visible', false);
+		setProperty('Cuphead.visible', true);
+	end
+
+	if value1 == 'Monika' then
+		setProperty('Cuphead.visible', false);
+		setProperty('Monika.visible', true);
+	end
+
+	if value1 == 'Sayori' then
+		setProperty('Monika.visible', false);
+		setProperty('Sayori.visible', true);
+		setProperty('Poem.visible', true);
+
+	end
+
+	if value1 == 'Natsuki' then
+		setProperty('Sayori.visible', false);
+		setProperty('Poem.visible', false);
+		setProperty('Natsuki.visible', true);
+		setProperty('Static.visible', true);
+	end
+
+	if value1 == 'Yuri' then
+		setProperty('Natsuki.visible', false);
+		setProperty('Static.visible', false);
+		setProperty('Yuri.visible', true);
+	end
+
+	if value1 == 'Cold' then
+		setProperty('Yuri.visible', false);
+		setProperty('Cold.visible', true);
+	end
+
+	if value1 == 'Final' then
+		setProperty('Cold.visible', false);
+		setProperty('Sans.visible', true);
+		setProperty('nat.visible', true);
+		setProperty('Static2.visible', true);
+		setProperty('Barrer3.visible', true);
+	end
+
+	if value1 == 'Final2' then
+		setProperty('Sans.visible', false);
+		setProperty('nat.visible', false);
+		setProperty('Static2.visible', false);
+		setProperty('Cup.visible', true);
+		setProperty('Sayo.visible', true);
+		setProperty('Poem2.visible', true);
+	end
+	
+	if value1 == 'Final3' then
+		setProperty('Barrer3.visible', false);
+		setProperty('Cup.visible', false);
+		setProperty('Sayo.visible', false);
+		setProperty('Poem2.visible', false);
+		setProperty('Ben.visible', true);
+		setProperty('BendyNight.visible', true);
+	end
+
+	if value1 == 'Marcos' then
+		setProperty('Ben.visible', false);
+		setProperty('BendyNight.visible', false);
+		setProperty('Marcos.visible', true);
+	end
+end
+end
